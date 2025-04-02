5c2079ff6167a6719431926e33502e62047deba0
justin soni <justisoni2026@mca.ajce.in>
2025-04-02 10:30:24 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/header';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import AddTask from './components/addtask';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AddTask from './components/Addtask';
 
 
 function App() {
